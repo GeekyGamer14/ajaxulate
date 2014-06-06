eb855407c058fc2ba901605a0b911fb189066c10
TJ Horner <GeekyGamer14@iQuestria.net>
2014-06-06 10:48:26 -0700

    Update ajaxulate.js

--- ajaxulate.js
+++ ajaxulate.js
@@ -1,27 +1,22 @@
-var ajaxulate;
-ajaxulate = {
-	post: function(url, data, callback){
-		$.ajax({
-			url: url,
-			type: "POST",
-			data: data,
-			success: callback,
-			error: callback,
-			done: callback
-		});
-	},
-	get: function(url, data, callback){
-		$.ajax({
-			url: url,
-			type: "GET",
-			data: data,
-			success: callback,
-			error: callback,
-			done: callback
-		});
-	}
+var Ajaxulate;
+Ajaxulate = {
+	i: [],
+	c: function(){return;}
 };
 
-if(!window.jQuery){
-	console.error("jQuery was not detected, ajaxulate will not load correctly.");
+Ajaxulate.init = function(i,c){
+	c = c || function(){return;};
+	i = i || [];
+	Ajaxulate.i = i;
+	Ajaxulate.c = c;
+}
+
+Ajaxulate.search = function(i,c){
+	c = c || Ajaxulate.c;
+	i = i || Ajaxulate.i;
+	document.getElementsByTagName('a').forEach(function(o){
+		Ajaxulate.i.forEach(function(i){
+			// todo if(o.getAttribute('data-ajaxulate').indexOf(i.url))	
+		});
+	});
 }
